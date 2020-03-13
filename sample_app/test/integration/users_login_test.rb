1dfc0cad9e434a507355900b9ad3eec80931e0c1
出口風人 <kazato@KazatoDeguchi.local>
2020-03-13 23:25:29 +0900

    commit 11th

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @non_activated_user = users(:hoge)
   end
 
   test "login with invalid information" do
@@ -46,4 +47,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     log_in_as(@user, remember_me: '0')
     assert_empty cookies['remember_token']
   end
+
+  test "login as non-activated user" do
+    log_in_as(@non_activated_user)
+    assert_redirected_to root_url
+  end
+
 end
